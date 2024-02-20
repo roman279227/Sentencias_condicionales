ad346b349d457cfbff2ef463ea7a086a4e78777b
Roman <danielfernandezroman@gmail.com>
2024-02-20 19:02:04 +0100

    if con una o varias sentencias

--- src/sentencias_condicionales/Estructura_if.java
+++ src/sentencias_condicionales/Estructura_if.java
@@ -10,11 +10,32 @@ public class Estructura_if {
     public static void main(String[] args) {
          
         
-    int edad=15;
+    int edad=21;
+    double altura=1.8;
     
     if(edad>18)
+        
+    System.out.println("Bienvenido ");
     
-    System.out.println("Bienvenido");
+    
+    if(altura<2){
+        
+        System.out.println("Tranquilizate, ya llegarás a la altura,o no");
+        
+        System.out.println("Come más Danoninos");
+        
+        
+        
+    }    
+        
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     }
