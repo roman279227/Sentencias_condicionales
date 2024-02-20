bfe975ebd1a61e9135628c88dd488c7c2b88c1fe
Roman <danielfernandezroman@gmail.com>
2024-02-20 18:33:21 +0100

    Pequeño código en ejemplo if

--- src/sentencias_condicionales/Estructura_if.java
+++ src/sentencias_condicionales/Estructura_if.java
@@ -7,9 +7,22 @@ public class Estructura_if {
     
     
     
+    public static void main(String[] args) {
+         
+        
+    int edad=15;
+    
+    if(edad>18)
+    
+    System.out.println("Bienvenido");
+    
+    
+    }
+
     
     
     
     
+     
     
 }
